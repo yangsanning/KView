40dde7cb1f555c7d63bd5bba2666742910494448
yangsanning <yangsanning@saiyaoyun.com>
2018-06-13 15:27:23 +0800

    [ +] 绘制横线

--- app/src/main/java/ysn/com/kview/KView.java
+++ app/src/main/java/ysn/com/kview/KView.java
@@ -2,7 +2,6 @@ package ysn.com.kview;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.PathEffect;
@@ -38,6 +37,11 @@ public class KView extends View {
     private int column = 2;
 
     /**
+     * 横数
+     */
+    private int row = 2;
+
+    /**
      * 虚线效果
      */
     private PathEffect mDashEffect = DEFAULT_DASH_EFFECT;
@@ -57,14 +61,17 @@ public class KView extends View {
         super.onDraw(canvas);
         int viewHeight = getHeight();
         int viewWidth = getWidth();
-        float longitudeSpacing = (viewWidth - 2 * textSpace) / column;
+        float columnSpacing = (viewWidth - 2 * textSpace) / column;
+        float rowSpacing = viewHeight / row;
 
         // 绘制边框
         drawBorders(canvas, viewHeight, viewWidth);
 
+        // 绘制竖线
+        drawLongitudes(canvas, viewHeight, columnSpacing);
 
-        // 绘制经线
-        drawLongitudes(canvas, viewHeight, longitudeSpacing);
+        // 绘制横线
+        drawLatitudes(canvas, viewHeight, viewWidth, rowSpacing);
     }
 
 
@@ -73,7 +80,7 @@ public class KView extends View {
      */
     private void drawBorders(Canvas canvas, int viewHeight, int viewWidth) {
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setColor(ResUtil.getColor(R.color.k_view_row));
+        paint.setColor(ResUtil.getColor(R.color.k_view_frame));
         paint.setStrokeWidth(1);
         canvas.drawLine(textSpace, 1, viewWidth - textSpace, 1, paint);
         canvas.drawLine(viewWidth - textSpace, viewHeight - 1, textSpace, viewHeight - 1, paint);
@@ -81,17 +88,36 @@ public class KView extends View {
     }
 
     /**
-     * 绘制经线
+     * 绘制竖线
      */
-    private void drawLongitudes(Canvas canvas, int viewHeight, float longitudeSpacing) {
+    private void drawLongitudes(Canvas canvas, int viewHeight, float space) {
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(ResUtil.getColor(R.color.k_view_column));
         paint.setStrokeWidth(1);
         paint.setPathEffect(mDashEffect);
         setLayerType(LAYER_TYPE_SOFTWARE, null);
         for (int i = 1; i < column; i++) {
-            canvas.drawLine(textSpace + longitudeSpacing * i, 1, textSpace + longitudeSpacing * i,
+            canvas.drawLine(textSpace + space * i, 1, textSpace + space * i,
                     viewHeight - 1, paint);
         }
+    }
+
+    /**
+     * 绘制横线
+     *
+     * @param canvas
+     * @param viewHeight
+     * @param viewWidth
+     */
+    private void drawLatitudes(Canvas canvas, int viewHeight, int viewWidth, float space) {
+        Paint paint = new Paint();
+        paint.setColor(ResUtil.getColor(R.color.k_view_row));
+        paint.setPathEffect(mDashEffect);
+
+        for (int i = 1; i < row; i++) {
+            canvas.drawLine(textSpace, viewHeight + 1 - space * i, viewWidth - textSpace,
+                    viewHeight + 1 - space * i, paint);
+        }
+
     }
 }
